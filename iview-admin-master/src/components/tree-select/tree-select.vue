16b9d39a04ba445107fd775bd46793030cbd0041
ChenZiyi921 <919105896@qq.com>
2021-01-15 14:47:20 +0800

    升级4.0

--- iview-admin-master/src/components/tree-select/tree-select.vue
+++ iview-admin-master/src/components/tree-select/tree-select.vue
@@ -17,10 +17,10 @@
 </template>
 
 <script>
-import Emitter from 'iview/src/mixins/emitter'
-import TreeSelectTreeItem from './tree-select-tree.vue'
+import Emitter from "view-design/src/mixins/emitter";
+import TreeSelectTreeItem from "./tree-select-tree.vue";
 export default {
-  name: 'TreeSelect',
+  name: "TreeSelect",
   mixins: [Emitter],
   components: {
     TreeSelectTreeItem
@@ -36,31 +36,34 @@ export default {
     },
     loadData: Function
   },
-  data () {
+  data() {
     return {
       isChangedByTree: true,
       isInit: true
-    }
+    };
   },
-  provide () {
+  provide() {
     return {
       parent: this
-    }
+    };
   },
   methods: {
-    handleChange (selected) {
-      if (!this.isChangedByTree) this.$emit('input', selected)
-      this.isChangedByTree = false
+    handleChange(selected) {
+      if (!this.isChangedByTree) this.$emit("input", selected);
+      this.isChangedByTree = false;
     },
-    handleTreeCheck (selectedArray) {
-      this.isChangedByTree = true
-      this.$emit('input', selectedArray.map(item => item.id))
+    handleTreeCheck(selectedArray) {
+      this.isChangedByTree = true;
+      this.$emit(
+        "input",
+        selectedArray.map(item => item.id)
+      );
     },
-    handleClear () {
-      this.$refs.select.reset()
+    handleClear() {
+      this.$refs.select.reset();
     }
   }
-}
+};
 </script>
 
 <style lang="less">
--- iview-admin-master/src/libs/axios.js
+++ iview-admin-master/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import store from '@/store'
-// import { Spin } from 'iview'
+// import { Spin } from 'view-design'
 const addErrorLog = errorInfo => {
   const { statusText, status, request: { responseURL } } = errorInfo
   let info = {
@@ -13,11 +13,11 @@ const addErrorLog = errorInfo => {
 }
 
 class HttpRequest {
-  constructor (baseUrl = baseURL) {
+  constructor(baseUrl = baseURL) {
     this.baseUrl = baseUrl
     this.queue = {}
   }
-  getInsideConfig () {
+  getInsideConfig() {
     const config = {
       baseURL: this.baseUrl,
       headers: {
@@ -26,13 +26,13 @@ class HttpRequest {
     }
     return config
   }
-  destroy (url) {
+  destroy(url) {
     delete this.queue[url]
     if (!Object.keys(this.queue).length) {
       // Spin.hide()
     }
   }
-  interceptors (instance, url) {
+  interceptors(instance, url) {
     // 请求拦截
     instance.interceptors.request.use(config => {
       // 添加全局的loading...
@@ -64,7 +64,7 @@ class HttpRequest {
       return Promise.reject(error)
     })
   }
-  request (options) {
+  request(options) {
     const instance = axios.create()
     options = Object.assign(this.getInsideConfig(), options)
     this.interceptors(instance, options.url)
