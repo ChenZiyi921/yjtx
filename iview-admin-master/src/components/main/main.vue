3f5a6556424eeba2e1e9056c8bc4e7bf8c39af85
ChenZiyi921 <919105896@qq.com>
2021-01-15 14:38:10 +0800

    修改布局

--- iview-admin-master/src/components/main/main.vue
+++ iview-admin-master/src/components/main/main.vue
@@ -16,12 +16,13 @@
         :collapsed="collapsed"
         @on-select="turnToPage"
         :menu-list="menuList"
+        style="padding-top: 20px;"
       >
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
-        <div class="logo-con">
+        <!-- <div class="logo-con">
           <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
           <img v-show="collapsed" :src="minLogo" key="min-logo" />
-        </div>
+        </div> -->
       </side-menu>
     </Sider>
     <Layout>
